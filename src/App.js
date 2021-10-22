e89d83a98a5bbf5d28580f0066b46d7b047d25eb
Davi Teixeira <daviteixeira.btm@gmail.com>
2021-10-22 03:35:27 -0300

    Finalização do tutorial - Criação do jogo da velha com React

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import styles from './App.css';
+import Game from './components/Game';
+
+function App() {
+  return (
+    <div className={styles.App}>
+      <Game />
+    </div>
+  );
+}
+
+export default App;
